aa2aaf559f8ff4d2f438f2c8581aee9b7b462fba
shahnaz200git config --global user.name nameshahnaz200 <ueshvd@yahoo.com>
2022-05-23 17:08:45 +0200

    i finished exercise num2
    goodboy-oldboy

---
+++ javascript/javascript1/week1/goodboy-oldboy.js
@@ -0,0 +1,22 @@
+//your dog year is 10 human years
+const dogYearOfBirth = 2017
+const dogYearFuture = 2027
+let shouldShowResultInDogYears = true
+const dogYear = dogYearFuture - dogYearOfBirth
+if (shouldShowResultInDogYears) {
+ console.log(
+   `Your dog will be ${dogYear * 10} dog years old in ${dogYearFuture}`
+ );
+} else {
+ console.log(
+   `Your dog will be ${dogYear} human years old in ${dogYearFuture}`
+ );
+}
+
+
+
+
+
+
+
+
