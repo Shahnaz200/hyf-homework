a347c86dc3c3399b0fba7d205235024687e63e73
shahnaz200git config --global user.name nameshahnaz200 <ueshvd@yahoo.com>
2022-05-24 17:50:59 +0200

    age-ify.js
    finished exercise num1

---
+++ javascript/javascript1/week1/age-ify.js
@@ -0,0 +1,16 @@
+const yearOfBirth = 1987
+let yearFuture = 2045
+let age = (yearFuture - yearOfBirth)
+
+
+console.log(yearOfBirth)
+console.log(yearFuture)
+console.log(yearFuture - yearOfBirth)
+
+
+console.log("You will be 40 years old in 2027")
+
+age = 40
+yearFuture = 2027
+
+console.log(yearFuture-yearOfBirth)
