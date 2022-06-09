05a15a11430087339acd5933ed066a160ba48c20
shahnaz200git config --global user.name nameshahnaz200 <ueshvd@yahoo.com>
2022-06-09 21:04:19 +0200

    exercise num 4 finito

---
+++ javascript/javascript1/week3/NOnoN0nOYes (Note taking app).js
@@ -0,0 +1,47 @@
+const notes = [];
+
+function saveNote(content, id) {
+ return notes.push({ content, id });
+
+}
+
+saveNote("Pick up groceries", 1);
+saveNote("Do laundry", 2);
+saveNote("Eat my mommy", 3);
+saveNote("Eat zebra", 4);
+saveNote("Check my homework", 5);
+saveNote("Go to bathroom", 6);
+saveNote("Keep calm and drink caffe", 7);
+saveNote("My mom is stupid from Aria", 8);
+saveNote("Cook my dog that doesn't exist", 7);
+saveNote("Play on my moms belly", 7);
+
+
+console.log(notes); // [{content: 'Pick up groceries', id: 1}, {content: 'Do laundry', id: 2}]
+
+
+function getNote(id) {
+ if (typeof id !== "number" )
+ return console.log("Error");
+
+else {
+
+ for ( let i = 0; i <= notes.length; i++){
+  if (id == notes[i].id){
+return console.log(notes[i]);
+   }
+  }
+ }
+}
+
+getNote(5);
+
+
+function logOutNotesFormatted() {
+ for ( let i = 0; i < notes.length; i++){ 
+  console.log(`The note with id: ${notes[i].id}, has the following note text: ${notes[i].content}.`)
+ }
+
+}
+
+logOutNotesFormatted(); 
