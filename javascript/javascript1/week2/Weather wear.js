aec2aeaf41e812c44d7960a845df6ed443f3a9d2
shahnaz200git config --global user.name nameshahnaz200 <ueshvd@yahoo.com>
2022-06-11 22:18:53 +0200

    updated Waether waer.js
    :(

--- javascript/javascript1/week2/Weather wear.js
+++ javascript/javascript1/week2/Weather wear.js
@@ -1,17 +1,17 @@
-function youCreateThisFunctionName(tempurture) {
- if (tempurture < 0) {
+function youCreateThisFunctionName(tempurature) {
+ if (tempurature < 0) {
      return "winter jacket ok";
- } else if (tempurture <= 12) {
+ } else if (tempurature <= 12) {
      return "a hat and shorts are ok with a normal jacket";
- } else if (tempurture <= 17) {
-     return "shorts and a sommer hat and a t shirt ok";
- } else if (tempurture <= 26) {
-     return "t shirt and underpants are ok";
- } else if (tempurture <= 37) {
-     return "its really hot maybe swimming shorts and a t shirt ok";
+ } else if (tempurature <= 17) {
+     return "shorts and a sommer hat and a t-shirt ok";
+ } else if (tempurature <= 26) {
+     return "t-shirt and underpants are ok";
+ } else if (tempurature <= 37) {
+     return "its really hot maybe swimming shorts and a t-shirt ok";
  } else {
-     return "nothing u die lol";
+     return "nothing?? you die lol";
  }
 };
 var clothesToWear = youCreateThisFunctionName(18);
-console.log(clothesToWear); // Logs out: t shirt and underpants are ok"
+console.log(clothesToWear); // Logs out: t-shirt and underpants are ok"
